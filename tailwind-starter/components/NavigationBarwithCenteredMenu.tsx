3d6f7f108a59d6613359b50eb2df84aa7bcb1c20
Tanveer88-88 <sitz_tani@hotmail.com>
2023-03-22 16:50:06 +0500

    image loaded

--- tailwind-starter/components/NavigationBarwithCenteredMenu.tsx
+++ tailwind-starter/components/NavigationBarwithCenteredMenu.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import CIRCLEAI from "@/shared/Images/circleai_dm9slt.png"
+
 const NavigationBarwithCenteredMenu = () => {
   return (
 
       <div className="bg-slate-100 max-w-5xl my-8 mx-auto">
         <div>
-          <Image src={""} alt=""></Image>
-          <span></span>
+          <Image src={CIRCLEAI} alt=""></Image>
         </div>
         <div className="">
           <Link href={"#"}>Home</Link>
